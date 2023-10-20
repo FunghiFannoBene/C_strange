1d09ca9cf3914ac92d292d5d73af10f5f882317c
FunghiFannoBene <119907315+FunghiFannoBene@users.noreply.github.com>
2023-10-20 19:56:34 +0200

    Create Static_mem_static_var.c

---
+++ Static_mem_static_var.c
@@ -0,0 +1,14 @@
+Non è possibile utilizzare una variabile int per dichiarare la dimensione di un array statico perché la dimensione deve essere nota a tempo di compilazione.
+static char	buf[BUFFER_SIZE + 1];
+inizializza già tutto a 0 se la variabile è statica.
+
+
+  Vantaggi:
+Persistenza: Mantengono il loro stato tra le chiamate alla funzione.
+Inizializzazione a Zero: Sono automaticamente inizializzate a zero se non altrimenti specificato.
+Riduzione dello Stack: Poiché sono allocate nella memoria statica, non utilizzano lo stack della funzione.
+Svantaggi:
+Condivisione di Stato: Possono introdurre effetti collaterali e rendere il codice più difficile da capire e testare.
+Thread Safety: Non sono sicure in un ambiente multithread a meno che non vengano gestite appropriatamente.
+Flessibilità: La dimensione è fissata a tempo di compilazione, quindi non possono essere ridimensionate dinamicamente come gli array dinamici.
+Usare variabili statiche può semplificare la gestione della memoria in alcuni casi, ma può anche introdurre complessità e errori se non gestite con attenzione. Scegli il tipo di variabile più appropriato al tuo caso specifico.
